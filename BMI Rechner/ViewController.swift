d6f16a29d9415faea92233ae50734e3c7f9d026a
Julius Schmid <schmid.julius@icloud.com>
2021-07-10 16:05:46 +0200

    Initial Commit

---
+++ BMI Rechner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Rechner
+//
+//  Created by JS on 10.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
